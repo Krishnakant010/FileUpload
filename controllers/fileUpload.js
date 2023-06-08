41fe99e5bf6d636ae02e5206afc8fff7e0e8b41d
Krishnakant010 <krishnakantgangurde0109@gmail.com>
2023-06-08 17:22:13 +0530

    CloudinaryImageUploade _Code

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,4 +1,5 @@
 const File = require("../models/File");
+const cloudinary = require("cloudinary").v2;
 // local file upload handler
 exports.localFileUpload = async (req, res) => {
   try {
@@ -17,5 +18,54 @@ exports.localFileUpload = async (req, res) => {
   } catch (err) {
     console.log("Error uploading file");
     console.log(err);
+  }
+};
+
+function isSupported(type, supportedTypes) {
+  return supportedTypes.includes(type);
+}
+
+async function uploadFileToCloudinary(file, folder) {
+  const options = { folder };
+  return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+
+// image upload handler
+exports.imageUpload = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+    const file = req.files.imageFile;
+    console.log(file);
+    //validate the file
+    const supportedTypes = ["jpg", "png", "jpeg"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+    console.log(fileType);
+    if (!isSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid file extension or it is not supported",
+      });
+    }
+    // now here it is supported
+    const response = await uploadFileToCloudinary(file, "krishnakant");
+
+    // entry in database;
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+    res.json({
+      success: true,
+      url: response.secure_url,
+      message: "Imagage uploaded successfully",
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
   }
 };
