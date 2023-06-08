19b8d62a9a94191b212c8c0899b9b00fa289ae32
Krishnakant010 <krishnakantgangurde0109@gmail.com>
2023-06-08 21:50:16 +0530

    Vdeo and image size reducer controllers

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -25,8 +25,13 @@ function isSupported(type, supportedTypes) {
   return supportedTypes.includes(type);
 }
 
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder, quality) {
   const options = { folder };
+  if (quality) {
+    options.quality = quality;
+  }
+
+  options.resource_type = "auto"; //This will avodi video upload error
   return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
@@ -49,6 +54,84 @@ exports.imageUpload = async (req, res) => {
     }
     // now here it is supported
     const response = await uploadFileToCloudinary(file, "krishnakant");
+
+    // entry in database;
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+    res.json({
+      success: true,
+      url: response.secure_url,
+      message: "Imagage uploaded successfully",
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+// video upload
+
+exports.videoUpload = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+
+    const file = req.files.videoFile;
+    console.log("this is file data", file);
+    const supportedTypes = ["mp4", "mov", "gif"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+    console.log(fileType);
+    if (!isSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid file extension or it is not supported",
+      });
+    }
+    console.log("before cloudinar");
+    const response = await uploadFileToCloudinary(file, "krishnakant");
+    console.log("after cloudinar");
+    // entry in database;
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+    res.json({
+      success: true,
+      url: response.secure_url,
+      message: "Imagage uploaded successfully",
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+
+exports.imageSizeReducer = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+
+    const file = req.files.imageFile;
+
+    //validate the file
+    const supportedTypes = ["jpg", "png", "jpeg"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    if (!isSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid file extension or it is not supported",
+      });
+    }
+    // now here file format it is supported
+    const response = await uploadFileToCloudinary(file, "krishnakant", 10);
 
     // entry in database;
     const fileData = await File.create({
